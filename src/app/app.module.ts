6539a6d68f676868d4caf102659368a50802a287
Cyberkrane <cyberkrane@gmail.com>
2022-05-07 22:09:18 -0300

    primera entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { FooterComponent } from './footer/footer/footer.component';
+import { GifsModule } from './gifs/gifs.module';
+import { HistorialComponent } from './historial/historial/historial.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistorialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
